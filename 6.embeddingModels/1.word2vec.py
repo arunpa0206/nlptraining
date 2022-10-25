e8e5aa1ca23292885e3f8c44eaeb236b40bc8449
Ashutosh Raman <61544363+ashutosh0709@users.noreply.github.com>
2022-10-25 17:57:59 +0530

    Update 1.word2vec.py

--- 6.embeddingModels/1.word2vec.py
+++ 6.embeddingModels/1.word2vec.py
@@ -8,3 +8,18 @@ model = Word2Vec(sentences, min_count=1)
 words = list(model.wv.index_to_key)
 print(words)
 print(model.wv['bank'])
+#######################################################
+# Now using PCA to reduce dimensionality to 2, for plotting the points:
+from sklearn.decomposition import PCA
+from matplotlib import pyplot
+
+X = model.wv[list(model.wv.index_to_key)]
+pca = PCA(n_components=2)
+result = pca.fit_transform(X)
+
+# create a scatter plot of the projection
+pyplot.scatter(result[:, 0], result[:, 1])
+words = list(model.wv.index_to_key)
+for i, word in enumerate(words):
+	pyplot.annotate(word, xy=(result[i, 0], result[i, 1]))
+pyplot.show()
