0fe402005571c82c6a249870335111017382526c
Avinash Kumar <60811564+Avinash9k5r@users.noreply.github.com>
2022-10-25 20:02:54 +0530

    Create 2.addksmoothing.py

---
+++ 20.statisticalml/0.languagemodel/2.smoothing/2.addksmoothing.py
@@ -0,0 +1,28 @@
+# Next word prediction     
+# Add-k smoothing
+
+from textprocessing import freq_ui, freq_bi, freq_tri
+
+word1 = "ram"
+word2 = "plays"
+
+s = word1 + ' ' + word2
+print(s)
+
+def predict_next_word(word1, word2, s):
+  for i in range(1):
+      max_count = 0
+      next_word = ""
+      for curr_word in freq_ui:
+        
+        prob_val = (freq_tri[word1, word2, curr_word]+0.5)/(freq_bi[word1, word2]+6)
+        if(prob_val > max_count):
+          max_count = prob_val
+          next_word = curr_word
+      
+      s=s+' '+ next_word
+      print(s)
+      word1 = word2
+      word2 = next_word
+
+predict_next_word(word1, word2, s)
