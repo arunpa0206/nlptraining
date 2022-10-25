612afeec8e8a471c56c6e39eb2b7e91cb3c7d979
Ashutosh Raman <61544363+ashutosh0709@users.noreply.github.com>
2022-10-25 10:57:41 +0530

    Update 2.cosinesimilaritywithstrings.py

--- 4.vectorbasics/2.cosinesimilaritywithstrings.py
+++ 4.vectorbasics/2.cosinesimilaritywithstrings.py
@@ -1,7 +1,39 @@
-import spacy
-nlp = spacy.load('en_core_web_md')
+A = "I love data mining"
+B = "I hate data mining"
 
-token1 = nlp('cat')
-token2 = nlp('dog')
-  
-print("Similarity:", token1.similarity(token2))
+import numpy
+
+wordsA = A.lower().split()
+wordsB = B.lower().split()
+
+print("Sent-1   :  " + str(wordsA))
+print("Sent-2   :  " + str(wordsB))
+
+vocab = set(wordsA)
+vocab = vocab.union(set(wordsB))
+vocab = list(vocab)
+
+print("Vocab    :  " + str(vocab))
+
+
+vA = numpy.zeros(len(vocab), dtype=float)
+vB = numpy.zeros(len(vocab), dtype=float)
+
+for w in wordsA:
+    i = vocab.index(w)
+    vA[i] += 1
+    
+print("Vector-1 :  " + str(vA))
+
+for w in wordsB:
+    i = vocab.index(w)
+    vB[i] += 1
+    
+print("Vector-2 :  " + str(vB))
+
+
+cos_sim = numpy.dot(vA, vB) / (numpy.sqrt(numpy.dot(vA,vA)) * numpy.sqrt(numpy.dot(vB,vB)))
+
+
+
+print("\nCosine-Similarity : " + str(cos_sim))
