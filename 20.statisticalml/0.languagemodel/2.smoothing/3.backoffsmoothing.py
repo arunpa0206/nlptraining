2aa738e7366e3edada266a77f2ba6ce53eb1c96d
Avinash Kumar <60811564+Avinash9k5r@users.noreply.github.com>
2022-10-26 14:10:33 +0530

    Update 3.backoffsmoothing.py

--- 20.statisticalml/0.languagemodel/2.smoothing/3.backoffsmoothing.py
+++ 20.statisticalml/0.languagemodel/2.smoothing/3.backoffsmoothing.py
@@ -9,13 +9,19 @@ word2 = "likes"
 s = word1 + ' ' + word2
 print(s)
 
+def get_prob_by_applying_backoff_smoothing(word1, word2, curr_word, freq_ui, freq_bi, freq_tri):
+  if(freq_ui[word2]==0):
+    return 0
+  prob_val = (freq_bi[word2, curr_word])/(freq_ui[word2])
+  return prob_val
+
 def predict_next_word(word1, word2, s):
   for i in range(1):
       max_count = 0
       next_word = ""
       for curr_word in freq_ui:
         
-        prob_val = (freq_bi[word2, curr_word])/(freq_ui[word2])
+        prob_val = get_prob_by_applying_backoff_smoothing(word1, word2, curr_word, freq_ui, freq_bi, freq_tri)
         if(prob_val > max_count):
           max_count = prob_val
           next_word = curr_word
