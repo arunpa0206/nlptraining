28c61e72b7bbe44782a8cfa7f2dcfc6be5bdd3e5
Avinash Kumar <60811564+Avinash9k5r@users.noreply.github.com>
2022-10-26 14:08:09 +0530

    Update 2.addksmoothing.py

--- 20.statisticalml/0.languagemodel/2.smoothing/2.addksmoothing.py
+++ 20.statisticalml/0.languagemodel/2.smoothing/2.addksmoothing.py
@@ -9,13 +9,17 @@ word2 = "plays"
 s = word1 + ' ' + word2
 print(s)
 
+def get_prob_by_applying_addk_smoothing(word1, word2, curr_word, freq_ui, freq_bi, freq_tri):
+  prob_val = (freq_tri[word1, word2, curr_word]+0.5)/(freq_bi[word1, word2]+6)
+  return prob_val
+
 def predict_next_word(word1, word2, s):
   for i in range(1):
       max_count = 0
       next_word = ""
       for curr_word in freq_ui:
         
-        prob_val = (freq_tri[word1, word2, curr_word]+0.5)/(freq_bi[word1, word2]+6)
+        prob_val = get_prob_by_applying_addk_smoothing(word1, word2, curr_word, freq_ui, freq_bi, freq_tri)
         if(prob_val > max_count):
           max_count = prob_val
           next_word = curr_word
