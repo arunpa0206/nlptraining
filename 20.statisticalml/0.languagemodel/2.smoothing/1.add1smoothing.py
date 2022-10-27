45ab5ad6a7d6b49ca036243a6cbeed456b88d909
Avinash Kumar <60811564+Avinash9k5r@users.noreply.github.com>
2022-10-27 08:37:22 +0530

    Update 1.add1smoothing.py

--- 20.statisticalml/0.languagemodel/2.smoothing/1.add1smoothing.py
+++ 20.statisticalml/0.languagemodel/2.smoothing/1.add1smoothing.py
@@ -3,12 +3,6 @@
 
 from textprocessing import freq_ui, freq_bi, freq_tri
 
-word1 = "ram"
-word2 = "plays"
-
-s = word1 + ' ' + word2
-print(s)
-
 def get_prob_by_applying_add1_smoothing(word1, word2, curr_word, freq_ui, freq_bi, freq_tri):
   prob_val = (freq_tri[word1, word2, curr_word]+1)/(freq_bi[word1, word2]+6)
   return prob_val
@@ -27,5 +21,11 @@ def predict_next_word(word1, word2, s):
       print(s)
       word1 = word2
       word2 = next_word
+
+word1 = "ram"
+word2 = "plays"
+
+s = word1 + ' ' + word2
+print(s)
 
 predict_next_word(word1, word2, s)
