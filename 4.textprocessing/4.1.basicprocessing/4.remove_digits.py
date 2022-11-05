dad54e4db96e3c690bab85d4835c818c91cff6fb
Ashutosh Raman <61544363+ashutosh0709@users.noreply.github.com>
2022-11-06 01:54:21 +0530

    Update 4.remove_digits.py

--- 4.textprocessing/4.1.basicprocessing/4.remove_digits.py
+++ 4.textprocessing/4.1.basicprocessing/4.remove_digits.py
@@ -1,7 +1,14 @@
 input_txt = "Nice 2 see you. I will3 join you."
-#tokenize the input sentence using white spaces
-#check if the token contain digits
-#Join the tokens which do not contain digits using white space
-output_txt = ' '.join(s for s in input_txt.split() 
-                     if not any(c.isdigit() for c in s))
+output_txt = ''
+for token in input_txt.split():
+    flag = False #If flag == true for a token, then that token contains digit.
+    for char in token:
+        if(char.isdigit()):
+            flag = True
+            break
+    if(flag == True):
+        flag = False
+    else:    
+        output_txt += " " + token 
+
 print(output_txt)
