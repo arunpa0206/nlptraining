e9ab78e1c99490ec36b6059e721582dba059b6aa
Ashutosh Raman <61544363+ashutosh0709@users.noreply.github.com>
2022-11-13 12:51:53 +0530

    Update main.py

--- 9.applications/2.topicmodelling/main.py
+++ 9.applications/2.topicmodelling/main.py
@@ -4,32 +4,32 @@ from packages import preprocessing, generateWordEmbedding, elbow, clusteriseAndG
 ##########################################################################################
 # Data Preprocessing:
 
-data = _1_preprocessing.convertFileToVar('global_warming.txt') 
-data = _1_preprocessing.removespecialcharsAndNumbers(data)
-data = _1_preprocessing.tokenizeAndRemoveStopwordsAndSingleChar(data)
-data = _1_preprocessing.lemmatize(data)
+data = preprocessing.convertFileToVar('global_warming.txt') 
+data = preprocessing.removespecialcharsAndNumbers(data)
+data = preprocessing.tokenizeAndRemoveStopwordsAndSingleChar(data)
+data = preprocessing.lemmatize(data)
 lematized_list_cpy = data.copy()
 
 #########################################################################################
 # Generating Word-Vectors:
 
-tokenizer, model = _2_generateWordEmbedding.loadBERTModel()
-data, X = _2_generateWordEmbedding.getBERTVectors(data , model , tokenizer)
-df = _2_generateWordEmbedding.buildDFOfCurrentWordVectorRelation(data, X)
+tokenizer, model = generateWordEmbedding.loadBERTModel()
+data, X = generateWordEmbedding.getBERTVectors(data , model , tokenizer)
+df = generateWordEmbedding.buildDFOfCurrentWordVectorRelation(data, X)
 print(df.head())
 
 #########################################################################################
 # Getting No of clusters as 'k' in k-means clustering:
 
-elbow_val = _3_elbow.getElbowMethodGraph(X)
+elbow_val = elbow.getElbowMethodGraph(X)
 print("Elbow Value -> " + str(elbow_val))
 
 #########################################################################################
 # Clustering and getting top words as relevant topics:
 
-km, y = _4_clusteriseAndGetTopics.makeKClusters(elbow_val , X)
-closest_overall_output_words , words_by_cluster_no , final_list_of_used_words , X , list_all_words = _4_clusteriseAndGetTopics.getTopNounsFromEachCluster(km , data , X  , lematized_list_cpy , num_of_clusters = elbow_val)
-topic_words , words_by_cluster_no, words_by_cluster_no_with_distances = _4_clusteriseAndGetTopics.mergeNounsFromEachCluster(words_by_cluster_no , list_all_words)
+km, y = clusteriseAndGetTopics.makeKClusters(elbow_val , X)
+closest_overall_output_words , words_by_cluster_no , final_list_of_used_words , X , list_all_words = clusteriseAndGetTopics.getTopNounsFromEachCluster(km , data , X  , lematized_list_cpy , num_of_clusters = elbow_val)
+topic_words , words_by_cluster_no, words_by_cluster_no_with_distances = clusteriseAndGetTopics.mergeNounsFromEachCluster(words_by_cluster_no , list_all_words)
 print("Topics Obtained : \n" + str(set(topic_words)))
 
 #########################################################################################
