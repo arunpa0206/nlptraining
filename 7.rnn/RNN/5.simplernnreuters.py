5f73e333a6c414d1f32016d6544b0157207eb9dd
Avinash Kumar <avinash9k5r@gmail.com>
2023-01-21 18:32:02 +0530

    add rnn lstm gru for reuters database

---
+++ 7.rnn/RNN/5.simplernnreuters.py
@@ -0,0 +1,79 @@
+import keras
+
+from keras.layers import SimpleRNN
+from keras.models import Sequential
+from keras.layers import Embedding, SimpleRNN
+from keras.layers import Dense
+
+from keras.datasets import reuters
+from keras.preprocessing import sequence
+
+max_features = 10000  # number of words to consider as features
+maxlen = 500  # cut texts after this number of words (among top max_features most common words)
+batch_size = 64
+
+print('Loading data...')
+(input_train, y_train), (input_test, y_test) = reuters.load_data(num_words=max_features)
+print(len(input_train), 'train sequences')
+print(len(input_test), 'test sequences')
+
+print('Pad sequences (samples x time)')
+input_train = sequence.pad_sequences(input_train, maxlen=maxlen)
+input_test = sequence.pad_sequences(input_test, maxlen=maxlen)
+print('input_train shape:', input_train.shape)
+print('input_test shape:', input_test.shape)
+print('y_train shape:', y_train.shape)
+print('y_test shape:', y_test.shape)
+
+
+from keras.utils import np_utils
+
+y_train = np_utils.to_categorical(y_train, 46)
+y_test = np_utils.to_categorical(y_test, 46)
+y_test.shape
+
+
+from keras.models import Sequential
+from keras.layers import Embedding, SimpleRNN
+from keras.layers import Dense
+
+model = Sequential()
+model.add(Embedding(max_features, 64))
+model.add(SimpleRNN(64, return_sequences=True))
+model.add(SimpleRNN(64, return_sequences=True))
+model.add(SimpleRNN(64, return_sequences=True))
+model.add(SimpleRNN(64))
+model.add(Dense(46, activation='softmax'))
+
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['acc'])
+history = model.fit(input_train, y_train,
+                    epochs=10,
+                    batch_size=128,
+                    validation_split=0.3)
+
+
+model.summary()
+
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
