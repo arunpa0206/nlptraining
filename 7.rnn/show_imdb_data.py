8f466e4e49581a53a7d426c4a00d5f51e3aa512d
arunpa0206 <mail_arunn@yahoo.com>
2023-01-26 18:20:58 +0530

    Delete show_imdb_data.py

--- 7.rnn/show_imdb_data.py
+++
@@ -1,9 +0,0 @@
-from keras.datasets import imdb
-max_features = 20000
-print('Loading data...')
-(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
-
-# Printing first 5 data entries from imdb dataset:
-for i in range(5):
-    print(x_train[i])
-    print('-----------------------------------------------------------')
